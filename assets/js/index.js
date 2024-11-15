a569c0bd41738fc118dab967721cc12b0f25ba5e
Pedro Cavalcanti <pedropjc500@gmail.com>
2024-11-15 20:35:48 -0300

    add darkmode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Animations logic
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -23,3 +24,25 @@ function animateHeaderOnLoad() {
 
 document.addEventListener('DOMContentLoaded', animateHeaderOnLoad);
 window.addEventListener('scroll', animateOnScroll);
+
+
+// Darkmode logic
+const darkModeToggle = document.getElementById('darkmode-toggle');
+const inHome = document.getElementById('in-home');
+const socialMediaLinks = document.querySelectorAll('.social-media-link');
+const notInHomeLinks = document.querySelectorAll('.not-in-home-link');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('body-darkmode'); 
+    inHome.classList.toggle('in-home-darkmode');
+
+    notInHomeLinks.forEach(notInHomeLink => {
+        notInHomeLink.classList.toggle('not-in-home-link-darkmode');
+    });
+
+    darkModeToggle.classList.toggle('button-toggle-darkmode');
+    
+    socialMediaLinks.forEach(socialMediaLink => {
+        socialMediaLink.classList.toggle('social-media-link-darkmode');
+    });
+});
